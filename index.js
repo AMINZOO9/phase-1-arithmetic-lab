ffde509e485e05328b0df62d4307f14e8602b5d6
AMINZOO9 <aminaliabdi162@gmail.com>
2025-07-27 18:06:15 +0300

    initial update

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
 
 // Write your code here
+// Multiply
+let num1 = 31;
+let num2 = 2;
+let multiply = num1 * num2;
+
+// Random
+let random = Math.floor(Math.random() * 100) + 1; // Always > 0
+
+// Mod
+let num3 = 28;
+let num4 = 6;
+let mod = num3 % num4; // 28 % 6 === 4
+
+// Max
+let max = Math.max(1, 3, 5, 10, 20); // Highest is 20
